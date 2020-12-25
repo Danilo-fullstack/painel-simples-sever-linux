2205d7af1429777c2c68f6a4115dd42b5672966b
Danilo-fullstack <57022042+Danilo-fullstack@users.noreply.github.com>
2020-12-24 21:13:35 -0300

    update

--- _php/comando.php
+++ _php/comando.php
@@ -9,14 +9,88 @@
 // $connection = ssh2_connect($ip, $port);
 // ssh2_auth_password($connection, $user, $pass);
 
-echo ($_POST[comando]);
-if(empty($_POST[comando])) {
-    echo ('<strong><p>Campo INPUT vazio</p></strong>');
-    header('Location: ../painel.php');
-    exit();
-}
-$script = isset($_POST['comando']) ? $_POST['comando'] : '';
+// if(empty($_POST[comando])) {
+//     echo ('<strong><p>Campo INPUT vazio</p></strong>');
+//     header('Location: ../painel.php');
+//     exit();
+// }
+// $script = isset($_POST['comando']) ? $_POST['comando'] : '';
 
-$output = shell_exec($script);
-echo "<p style='color: red;'>$output</p>";
+// ---- Funções ----
+
+// Consulta data de vencimento de usuários
+function vencimentoUser($nameUser) {
+    $output = shell_exec("rm /var/www/html/_php/calcdata.txt");
+    $script = "grep '$nameUser' /etc/shadow | cut -d: -f8";
+    $output = shell_exec($script);
+    $output = shell_exec("date -d '1970/01/01 + $output days' > /var/www/html/_php/calcdata.txt");
+    $output = shell_exec("cut -d' ' -f2,3,6 /var/www/html/_php/calcdata.txt");
+    $output = shell_exec("date -d'$output' '+%d-%m-%Y'");
+    return $output;
+}
+// echo vencimentoUser("danilo.dev");
+
+// Faz uma lista com todos os usuarios(do grupo"18615")
+function listaUsers() {
+    shell_exec("rm /var/www/html/_php/listausers.txt");
+    shell_exec("grep '18615' /etc/shadow | cut -d: -f1 > /var/www/html/_php/listausers.txt");
+}
+
+// Conta linhas do arquivo listausers(OBS: requer que a listaUsers() tenha sido chamado primeiro)
+function contaLinhas() {
+    listaUsers();
+    $output = shell_exec("wc -l /var/www/html/_php/listausers.txt | cut -d' ' -f1");
+    $output = intval($output);
+    return $output;
+}
+// --------------------------------------------
+
+
+
+
+$rows = contaLinhas();
+$users_name = [];
+listaUsers();
+
+for($i = 1; $i < $rows; $i++) {
+    $users_name[$i] = shell_exec("sed -n '$i p' /var/www/html/_php/listausers.txt | cut -d: -f1");
+    echo "$users_name[$i] <br>";
+}
+// echo $users_name[];
+
+
+
+
+
+
+
+
+
+// $output = shell_exec("date -d '1970/01/01 + $output days'");
+// echo "$output <br>";
+
+// $output = shell_exec("date -d '$output' '+%Y-%m-%d'");
+// echo "$output<br><br>";
+
+// $output = shell_exec("cut -d' ' -f2,3,6 /var/www/html/_php/calcdata.txt");
+// echo "$output <br>";
+
+// function loopUsers() {
+
+// }
+
+// function showUsers() {
+    // $output = shell_exec('grep "18615" /etc/shadow | cut -d: -f1');
+    // echo "$output<br><br>";
+
+    // var_dump($output);
+
+    // $arrayUsers = explode(" ", $output);
+    // print $arrayUsers[3];
+//     return $arrayUsers;
+// }
+
+
+
+
 ?>
--- painel.php
+++ painel.php
@@ -1,25 +1,25 @@
 <?php
 session_start();
 include('./_php/verifica_login.php');
-// include('./_php/comando.php');
+include('./_php/comando.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel</title>
-    <style>
-        .box-teste {
-            color: red;
-        }
-    </style>
 </head>
 <body>
-    <form action="./_php/comando.php" method="post">
-        <input type="text" name="comando" value="ls">
+    <!-- <form action="./_php/comando.php" method="post">
+        <input type="text" name="comando" value="grep 'danilo.dev' /etc/shadow | cut -d: -f8">
         <input type="submit" value="Enviar Comando">
     </form>
-    <h2><a href="./_php/logout.php">Sair</a></h2>
+    <h2><a href="./_php/logout.php">Sair</a></h2> -->
+    <?php
+    //    showUsers();
+    //    var_dump($arrayUsers);
+    ?>
 </body>
 </html>
